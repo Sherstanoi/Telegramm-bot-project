4e3e05a71da10e76bb7021c252cf56e42ff1adad
DianaLoveCat <dinadiana222007@gmail.com>
2025-04-07 19:01:52 +0300

    Add files via upload

--- farm.cpp
+++ farm.cpp
@@ -3,24 +3,26 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <ctime>
 
 char name[100];
 int money = 200;
+time_t time_old = time (NULL);
+time_t time_new;
 
+void time() {
+    while (true){
+        int kolichestvo_dney;
 
-void start() {
-  std::cout << "Как вы назовете свою ферму: ";
-  std::cin >> name;
-  menu();
-}
-
-void OutputProperty() {
-    std::cout << "У вас есть: ";
-    std::cout << money << " монет\n" << ;
-}
-
-void menu() {
-    std::cout << "Добро пожаловать в " << name << '\n';
-    OutputProperty();
-
+        time_new = time (NULL);
+        //kolichestvo_dney = (time_new/3600)/24;
+        std::cout << "Сейчас: " << asctime(localtime(&time_new)) << "\nс прошлого обновления прошло: " << time_new - time_old << '\n';
+        time_old = time_new;
+        std::cin.get();
+        char a;
+        std::cin >> a;
+        if (a == 'y') {
+            break;
+        }
+    }
 }
